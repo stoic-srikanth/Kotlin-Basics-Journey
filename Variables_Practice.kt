c696e6f4cb02be543b399240987eae84b1c0b3de
stoic-srikanth <stoic.srikanth@gmail.com>
2026-04-20 20:41:00 +0530

    Completed Day 1: Basics of Kotlin Variables.
    Practiced using 'val' and 'var' in the Kotlin Playground and also the use of  increment operator.

---
+++ Variables_Practice.kt
@@ -0,0 +1,8 @@
+//To run the program which begins with initial value of 10 unread emails.
+//Further to get the output as 10 and use the increment operator ++ [or count + 1], and get the new output of 11 unread emails.
+fun main() {
+    var count = 10
+    println("You have $count unread emails.")
+    count++
+    println("You have $count unread emails.")
+}
